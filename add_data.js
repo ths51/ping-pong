e584bddcbe5a4c48c01b9ae34d8c727993843bf8
Eli W <elijahwolf05@icloud.com>
2024-01-28 23:05:52 -0600

    updated data, added view data page (without data)

--- add_data.js
+++ add_data.js
@@ -1,9 +1,16 @@
-import fs from 'fs';
+// import fs from 'fs';
 
-reader = new FileReader()
-file = fs.readFile('Ping Pong Stats.xlsx')
+// reader = new FileReader()
+// file = fs.readFile('Ping Pong Stats.xlsx')
 
+function add_data() {
+    let x = document.getElementById('server1').value + " - " + document.getElementById('s1_score').value
+    let y = document.getElementById('server2').value + " - " + document.getElementById('s2_score').value
+    console.log(x)
+    console.log(y)
+    return 0
+}
 
+add_data()
 
-
-console.log(reader)
+// console.log(reader)
